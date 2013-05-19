391ed03ad4c9bd455b0d317fe3eee4aed3b2a54e
root <root@racerx.(none)>
2013-05-19 01:11:34 -0400

    Added lock users to secsys module

--- createrepo.sh
+++ createrepo.sh
@@ -3,19 +3,19 @@
 ## CREATED BY TIM SKI | MARSHYSKI(DOT)COM ##
 ## MARSHYSKI[AT]GMAIL[DOT]COM             ##
 ## LAZY CREATEREPO SCRIPT                 ##
-## 05APR2013                              ##
+## 19MAY2013                              ##
 ## TESTED ON RHEL 5-6                     ##
 ############################################
 
 umask 0022
 
-CENTOS_64_OS="/home/www/html/CentOS/6.4/os/x86_64/Packages/"
-CENTOS_64_UP="/home/www/html/CentOS/6.4/updates/x86_64/Packages/"
+CENTOS_64_OS="/usr/share/nginx/html/CentOS/6.4/os/x86_64/Packages/"
+CENTOS_64_UP="/usr/share/nginx/html/CentOS/6.4/updates/x86_64/Packages/"
 CENTOS_64_DATA=`dirname "$CENTOS_64_OS"`/repodata
 CENTOS_64_DATA2=`dirname "$CENTOS_64_UP"`/repodata
 
-#CENTOS_59_OS="/home/www/html/CentOS/5.9/os/x86_64/Packages/"
-#CENTOS_59_UP="/home/www/html/CentOS/5.9/updates/x86_64/Packages/"
+#CENTOS_59_OS="/usr/share/nginx/html/CentOS/CentOS/5.9/os/x86_64/Packages/"
+#CENTOS_59_UP="/usr/share/nginx/html/CentOS/5.9/updates/x86_64/Packages/"
 #CENTOS_59_DATA=`dirname "$CENTOS_59_OS"`/repodata
 #CENTOS_59_DATA2=`dirname "$CENTOS_59_UP"`/repodata
 
