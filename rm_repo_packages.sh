bc4f6644303d8854f9e834ec64e752ab738314c0
root <root@racerx.(none)>
2013-05-18 23:40:49 -0400

    Added yumrepo module

--- rm_repo_packages.sh
+++ rm_repo_packages.sh
@@ -3,12 +3,18 @@
 ## CREATED BY TIM SKI | MARSHYSKI(DOT)COM ##
 ## MARSHYSKI[AT]GMAIL[DOT]COM             ##
 ## CLEAN UNWANTED YUM REPO PACKAGES       ##
-## 05APR2013                              ##
+## 18MAY2013                              ##
 ## TESTED ON RHEL 5-6                     ##
 ############################################
 
 RM_LIST="/root/randomz/configs/rm_list"
-REPO="/home/www/html/CentOS/"
+REPO="/usr/share/nginx/html/CentOS"
+
+if [[ ! -d $REPO ]]; then
+   echo "$REPO doesn't exist! Exiting..."
+   echo ""
+   exit
+fi
 
 while read line; do
 
